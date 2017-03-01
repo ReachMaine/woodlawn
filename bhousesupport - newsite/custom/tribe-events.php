41f8cc25c8e2b439188023a7ea9e98160ca93149
Linda Speight <linda@reachmaine.com>
2017-03-01 16:57:26 -0500

    Use new filters to events calendar shortcode
    s.t. we can style the duration time in Events Calendar short code

--- bhousesupport - newsite/custom/tribe-events.php
+++ bhousesupport - newsite/custom/tribe-events.php
@@ -7,14 +7,14 @@ function wlm_events_caveat() {
 	$evt_cost = tribe_get_cost();
 	if ( ($evt_cost) && ($evt_cost > 0) ) {
 		echo do_shortcode('[block id="under-events-with-cost"]');
-	} 
+	}
 }
 
 //show cost on admin page (usually hidden with events tickets plus)
 add_filter( 'tribe_events_admin_show_cost_field', '__return_true', 100 );
 
 /*** events shortcode  ***/
-// strip out the time from the ESC short code.  
+// strip out the time from the ESC short code.
 // assumes a comma as date, time separator & that there is no comma in Date format.
 function ecs_remove_time( $date ) {
 	if ( strpos( $date, ", ") !== FALSE ) {
@@ -23,3 +23,15 @@ function ecs_remove_time( $date ) {
 	return $date;
 }
 //add_filter( 'ecs_event_list_details', 'ecs_remove_time' );
+
+
+// change duration date in ecs to use div so we can style it.  now trying to do with filter,,,,
+add_filter('ecs_event_date_tag_start','reach_esc_event_date_start',10,3 );
+function reach_esc_event_date_start($text, $atts, $post) {
+    return '<div class="duration time">';
+}
+
+add_filter('ecs_event_date_tag_end', 'reach_esc_event_date_end', 10, 3 );
+function reach_esc_event_date_end($text, $atts, $post) {
+    return '</div>';
+}
